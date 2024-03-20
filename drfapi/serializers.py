e78221b6d80469b6bbf8127acf0cc3a2c516449b
bhargavsonagara <9828688989bs@gmail.com>
2024-03-20 13:00:34 +0530

    add validation to product serializer

--- drfapi/serializers.py
+++ drfapi/serializers.py
@@ -18,6 +18,15 @@ class CustomerModelSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(validators=[UniqueValidator(queryset=Customer.objects.all(),
                                                                message='Record with such email is already exists.')])
 
+    def __init__(self, *args, **kwargs):
+        super(CustomerModelSerializer, self).__init__(*args, **kwargs)
+        self.fields['name'].error_messages['blank'] = u'name cannot be blank.'
+        self.fields['name'].error_messages['required'] = u'name is required.'
+        self.fields['contact_number'].error_messages['blank'] = u'contact number cannot be blank.'
+        self.fields['contact_number'].error_messages['required'] = u'contact number is required.'
+        self.fields['email'].error_messages['blank'] = u'email cannot be blank.'
+        self.fields['email'].error_messages['required'] = u'email is required.'
+
     class Meta:
         model = Customer
         fields = ('id', 'name', 'contact_number', 'email', 'created_at')
@@ -47,8 +56,15 @@ class CustomerModelSerializer(serializers.ModelSerializer):
 
 class ProductModelSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=100, validators=[
-        UniqueValidator(queryset=Customer.objects.all(), message='Record with such product name is already exists.')])
+        UniqueValidator(queryset=Product.objects.all(), message='Record with such product name is already exists.')])
     weight = serializers.DecimalField(decimal_places=2, max_digits=4, help_text='25')
+
+    def __init__(self, *args, **kwargs):
+        super(ProductModelSerializer, self).__init__(*args, **kwargs)
+        self.fields['name'].error_messages['blank'] = u'name cannot be blank.'
+        self.fields['name'].error_messages['required'] = u'name is required.'
+        self.fields['weight'].error_messages['blank'] = u'weight cannot be blank.'
+        self.fields['weight'].error_messages['required'] = u'weight is required.'
 
     class Meta:
         model = Product
@@ -65,6 +81,13 @@ class ProductModelSerializer(serializers.ModelSerializer):
 class OrderItemSerializer(serializers.ModelSerializer):
     quantity = serializers.IntegerField(default=1)
 
+    def __init__(self, *args, **kwargs):
+        super(OrderItemSerializer, self).__init__(*args, **kwargs)
+        self.fields['product'].error_messages['blank'] = u'product cannot be blank.'
+        self.fields['product'].error_messages['required'] = u'product is required.'
+        self.fields['quantity'].error_messages['blank'] = u'quantity cannot be blank.'
+        self.fields['quantity'].error_messages['required'] = u'quantity is required.'
+
     class Meta:
         model = OrderItem
         fields = ('product', 'quantity',)
@@ -79,6 +102,17 @@ class OrderModelSerializer(serializers.ModelSerializer):
     order_item = OrderItemSerializer(many=True, required=True)
     order_date = serializers.DateField(input_formats=["%d/%m/%Y"], format='%d/%m/%Y', help_text='31/12/2023',
                                        default=datetime.date.today())
+
+    def __init__(self, *args, **kwargs):
+        super(OrderModelSerializer, self).__init__(*args, **kwargs)
+        self.fields['customer'].error_messages['blank'] = u'customer cannot be blank.'
+        self.fields['customer'].error_messages['required'] = u'customer is required.'
+        self.fields['order_date'].error_messages['blank'] = u'order date cannot be blank.'
+        self.fields['order_date'].error_messages['required'] = u'order date is required.'
+        self.fields['address'].error_messages['blank'] = u'address cannot be blank.'
+        self.fields['address'].error_messages['required'] = u'address is required.'
+        self.fields['order_item'].error_messages['blank'] = u'order item cannot be blank.'
+        self.fields['order_item'].error_messages['required'] = u'order item is required.'
 
     class Meta:
         model = Order
@@ -123,8 +157,15 @@ class ListOrderItemSerailizer(serializers.ModelSerializer):
         fields = ('id', 'product', 'quantity', 'created_at')
 
 
+class ListCustomerModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('id', 'name', 'contact_number', 'email', 'created_at')
+
+
 class ListAllOrderModelSerializer(serializers.ModelSerializer):
     order_items = ListOrderItemSerailizer(many=True, read_only=True)
+    customer = ListCustomerModelSerializer(read_only=True)
 
     class Meta:
         model = Order
